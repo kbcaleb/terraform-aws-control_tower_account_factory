94e197abbab450a48ae9ab2bfc56ef663aa5445d
Caleb Collins <kbcaleb@gmail.com>
2024-11-04 21:16:19 -0700

    Fix deprecated EIP call

--- modules/aft-account-request-framework/vpc.tf
+++ modules/aft-account-request-framework/vpc.tf
@@ -202,8 +202,8 @@ resource "aws_internet_gateway" "aft-vpc-igw" {
 }
 
 resource "aws_eip" "aft-vpc-natgw-01" {
-  count = var.aft_enable_vpc ? 1 : 0
-  vpc   = true
+  count   = var.aft_enable_vpc ? 1 : 0
+  domain  = vpc
 }
 
 resource "aws_eip" "aft-vpc-natgw-02" {
